a179d43a66893bf9a457723394b385aaa63561bf
Milan Jovanović <milanj1705@gmail.com>
2024-06-01 22:14:31 +0200

    Finish keyboard

--- Assets/Scripts/Exam/ExamManager.cs
+++ Assets/Scripts/Exam/ExamManager.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System;
 using System.Drawing;
 
-public class GenerateText : MonoBehaviour
+public class ExamManager : MonoBehaviour
 {
     [SerializeField]
     private TextMeshProUGUI target;
@@ -36,6 +36,26 @@ public class GenerateText : MonoBehaviour
     private string targetText;
     private string targetTextToDisplay;
     private Exam[] exams;
+
+    private Dictionary<char, char> keyboardMap = new Dictionary<char, char>
+        {
+            {'a', 'a'}, {'b', 'b'}, {'c', 'c'}, {'d', 'd'}, {'e', 'e'}, {'f', 'f'}, {'g', 'g'},
+            {'h', 'h'}, {'i', 'i'}, {'j', 'j'}, {'k', 'k'}, {'l', 'l'}, {'m', 'm'}, {'n', 'n'},
+            {'o', 'o'}, {'p', 'p'}, {'q', 'q'}, {'r', 'r'}, {'s', 's'}, {'t', 't'}, {'u', 'u'},
+            {'v', 'v'}, {'w', 'w'}, {'x', 'x'}, {'y', 'y'}, {'z', 'z'},
+            {'A', 'A'}, {'B', 'B'}, {'C', 'C'}, {'D', 'D'}, {'E', 'E'}, {'F', 'F'}, {'G', 'G'},
+            {'H', 'H'}, {'I', 'I'}, {'J', 'J'}, {'K', 'K'}, {'L', 'L'}, {'M', 'M'}, {'N', 'N'},
+            {'O', 'O'}, {'P', 'P'}, {'Q', 'Q'}, {'R', 'R'}, {'S', 'S'}, {'T', 'T'}, {'U', 'U'},
+            {'V', 'V'}, {'W', 'W'}, {'X', 'X'}, {'Y', 'Y'}, {'Z', 'Z'},
+            {'0', '0'}, {'1', '1'}, {'2', '2'}, {'3', '3'}, {'4', '4'}, {'5', '5'}, {'6', '6'},
+            {'7', '7'}, {'8', '8'}, {'9', '9'},
+            {'`', '`'}, {'-', '-'}, {'=', '='}, {'~', '~'},
+            {'!', '!'}, {'@', '@'}, {'#', '#'}, {'$', '$'}, {'%', '%'}, {'^', '^'}, {'&', '&'},
+            {'*', '*'}, {'(', '('}, {')', ')'}, {'_', '_'}, {'+', '+'},
+            {'[', '['}, {']', ']'}, {'\\', '\\'}, {'{', '{'}, {'}', '}'}, {'|', '|'},
+            {';', ';'}, {'\'', '\''}, {':', ':'}, {'"', '"'},
+            {',', ','}, {'.', '.'}, {'/', '/'}, {'<', '<'}, {'>', '>'}, {'?', '?'}, {' ', ' '}
+        };
 
     static System.Random rnd = new System.Random();
 
@@ -82,7 +102,7 @@ public class GenerateText : MonoBehaviour
 
     void HandleTempInput(char c)
     {
-        if (c == ' ' && !inputCorrect && inputText[inputText.Length - 1] == ' ') return;
+        if (c == ' ' && !inputCorrect && inputText.Length > 0 && inputText[inputText.Length - 1] == ' ') return;
 
         if (c != '\b')
             inputText += c;
@@ -134,5 +154,9 @@ public class GenerateText : MonoBehaviour
     Exam[] ReadExams()
     {
         return JsonUtility.FromJson<Exams>(examsAssset.text).exams;
+    }
+    public Dictionary<char, char> KeyboardMap
+    {
+        get { return keyboardMap; }
     }
 }
---
+++ Assets/Scripts/Exam/KeyManager.cs
@@ -0,0 +1,111 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class KeyManager : MonoBehaviour
+{
+
+    private Animator animator;
+    private TextMeshProUGUI text;
+
+    private string mapping;
+
+    [SerializeField]
+    private KeyCode keyCode;
+
+    [SerializeField]
+    private GameObject examManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = gameObject.GetComponent<Animator>();
+        text = GetComponentInChildren<TextMeshProUGUI>();
+        string code = keyCode.ToString().Replace("Alpha", "").Trim();
+        code = GetMapping(code);
+        if (examManager != null && keyCode.ToString().Length == 1)
+            mapping = examManager.GetComponent<ExamManager>().KeyboardMap.GetValueOrDefault(code[0], code[0]).ToString();
+        else
+            mapping = GetMapping(code);
+        if (text != null)
+        {
+            text.SetText(mapping + "");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(keyCode))
+        {
+            animator.Play("New State", -1, 0f); ;
+        }
+    }
+
+    string GetMapping(string mapping)
+    {
+        switch(mapping)
+        {
+            case "Backspace":
+                return "<-";
+            case "Control":
+                return "CTRL";
+            case "Escape":
+                return "ESC";
+            case "Tilde":
+                return "~";
+            case "BackQuote":
+                return "`";
+            case "Minus":
+                return "-";
+            case "Equals":
+                return "=";
+            case "LeftParen":
+                return "{";
+            case "RightParen":
+                return "}";
+            case "Backslash":
+                return "\\";
+            case "LeftBracket":
+                return "[";
+            case "RightBracket":
+                return "]";
+            case "Semicolon":
+                return ";";
+            case "Quote":
+                return "'";
+            case "Return":
+                return "Enter";
+            case "CapsLock":
+                return "Caps";
+            case "LeftShift":
+                return "Shift";
+            case "RightShift":
+                return "Shift";
+            case "LeftControl":
+                return "Ctrl";
+            case "RightControl":
+                return "Ctrl";
+            case "LeftWindows":
+                return "Win";
+            case "RightWindows":
+                return "Win";
+            case "LeftAlt":
+                return "Alt";
+            case "RightAlt":
+                return "Alt";
+            case "RightMeta":
+                return "Meta";
+            case "Comma":
+                return ",";
+            case "Period":
+                return ".";
+            case "Slash":
+                return "/";
+            default:
+                return mapping;
+        }
+    }
+}
