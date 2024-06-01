9ee82c297f99c2271800b7e2f61deb10eb20d5fc
Boris Antonijev <81489899+fearwarden@users.noreply.github.com>
2024-06-01 12:10:53 +0200

    test

--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Test : MonoBehaviour
@@ -7,7 +5,7 @@ public class Test : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //Test
+        
     }
 
     // Update is called once per frame
