dcc99c5d6908ac4cfb48777b1a543498e392dabf
gase273 <39773535+gase273@users.noreply.github.com>
2024-06-01 19:53:39 +0200

    objedinjeni bg i study script, 95% gotovo

--- Assets/Scripts/Study/StudyScript.cs
+++ Assets/Scripts/Study/StudyScript.cs
@@ -17,6 +17,18 @@ public class StudyScript : MonoBehaviour
 
     private int currentScore = 0;
 
+    [SerializeField]
+    private SpriteRenderer spriteRenderer;
+
+    [SerializeField]
+    private Player player;
+
+    void darkenBackground()
+    {
+        spriteRenderer.color = new Color(player.Focus / 100f, player.Focus / 100f, player.Focus / 100f);
+        player.DecrementFocus(4);
+    }
+
     void generateLetter()
     {
         string chars = "1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ";
@@ -34,8 +46,10 @@ public class StudyScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        score.SetText(currentScore.ToString() + "/" + scoreGoal.ToString());
         InvokeRepeating("generateLetter", 0f, 2.5f);
         InvokeRepeating("timeOut", 2.0f, 2.0f);
+        InvokeRepeating("darkenBackground", 0f, 5f);
     }
 
     // Update is called once per frame
@@ -55,6 +69,7 @@ public class StudyScript : MonoBehaviour
             }
             else
             {
+                CancelInvoke();
                 Debug.Log("SUCCESS!");
             }
         }
