129fab1db4341688a769e6e59f8495fe4ec63098
gase273 <39773535+gase273@users.noreply.github.com>
2024-06-02 12:32:26 +0200

    main meni

--- Assets/Scripts/MainMenuScript.cs
+++ Assets/Scripts/MainMenuScript.cs
@@ -1,18 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenuScript : MonoBehaviour
 {
-    public void PlayGame()
+    public void PlayGame(int difficulty)
     {
-        //TODO: Load home scene
-        //SceneManager.LoadSceneAsync()
+        switch (difficulty)
+        {
+            case 0: //budzet
+                {
+                    //TODO add difficulty
+                    break;
+                }
+            case 1: //samofinansiranje
+                {
+                    //TODO: add difficulty
+                    break;
+                }
+            case 2: //burzuj
+                {
+                    //TODO: add difficulty
+                    break;
+                }
+                SceneManager.LoadSceneAsync("WorldScene");
+        }
     }
 
     public void QuitGame()
     {
-        Debug.Log("clicked");
         Application.Quit();
     }
 }
